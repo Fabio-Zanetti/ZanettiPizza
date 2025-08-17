8479c948f87212925f98e3bb500a4ba929259f70
Fabio Zanetti <fazanetti@gmail.com>
2025-08-17 16:28:55 -0300

    JS-Pizzas

---
+++ pizzas.js
@@ -0,0 +1,9 @@
+let pizzaJson = [
+    {id:1, name:'Mussarela', img:'images/pizza.png', price:45.50, sizes:['100g', '530g', '860g'], description:'Pizza clássica com molho de tomate, muçarela derretida, massa crocante e toque especial de orégano.'},
+    {id:2, name:'Calabresa', img:'images/pizza2.png', price:58.00, sizes:['320g', '530g', '860g'], description:'Pizza saborosa com calabresa fatiada, muçarela derretida, molho de tomate, cebola e toque de orégano.'},
+    {id:3, name:'Quatro Queijos', img:'images/pizza3.png', price:87.50, sizes:['320g', '530g', '860g'], description:'Pizza cremosa com muçarela, provolone, parmesão e gorgonzola, sobre massa crocante e molho suave.'},
+    {id:4, name:'Americana', img:'images/pizza4.png', price:49.00, sizes:['320g', '530g', '860g'], description:'Pizza estilo americana com muçarela, bacon, calabresa, milho, molho de tomate, cebola e toque especial.'},
+    {id:5, name:'Sorvete', img:'images/pizza5.png', price:55.00, sizes:['320g', '530g', '860g'], description:'Pizza doce com base crocante, sorvete cremoso, cobertura de chocolate, frutas frescas e confeitos coloridos.'},
+    {id:6, name:'Moda da Casa', img:'images/pizza6.png', price:88.50, sizes:['320g', '530g', '860g'], description:'Pizza especial com muçarela, calabresa, bacon, cebola, pimentão, azeitonas, molho caseiro e toque exclusivo.'},
+    {id:7, name:'Chocolate', img:'images/pizza7.png', price:55.50, sizes:['320g', '530g', '860g'], description:'Pizza doce com creme de chocolate, massa crocante, morangos frescos, confeitos coloridos e toque de chantilly.'}
+];
